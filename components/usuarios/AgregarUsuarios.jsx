143d2db02b2d90da663f27ef5ca002b7197bd904
lizy <liizgatica.01@gmail.com>
2022-04-17 19:58:45 -0500

    actualizacion tabla usuarios

---
+++ components/usuarios/AgregarUsuarios.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AgregarUsuarios = () => {
+  return (
+    <>
+      <div>
+        <input type="text" className="form-control" />
+      </div>
+    </>
+  );
+};
+
+export default AgregarUsuarios;
---
+++ pages/usuarios/index.jsx
@@ -0,0 +1,11 @@
+import AgregarUsuarios from "../../components/usuarios/AgregarUsuarios";
+
+const index = () => {
+  return (
+    <>
+      <AgregarUsuarios/>
+    </>
+  );
+};
+
+export default index;
