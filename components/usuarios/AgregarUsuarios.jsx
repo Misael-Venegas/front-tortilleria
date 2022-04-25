aebec7ce6763a01b78e92f2d4cba9ae6482d37f1
lizy <liizgatica.01@gmail.com>
2022-04-24 19:22:11 -0500

    vista usuarios

--- components/usuarios/AgregarUsuarios.jsx
+++ components/usuarios/AgregarUsuarios.jsx
@@ -1,31 +1,63 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Container from "../principal/Container";
+import { useLazyQuery, gql } from "@apollo/client";
+
+const OBTNER_USUARIOS = gql`
+query getUsuarios {
+  getUsuarios {
+    id
+    nombre
+    apellidoP
+    apellidoM
+    telefono
+    email
+    password
+    tipo
+  }
+}
+`
 
 const AgregarUsuarios = () => {
+
+  const [perro, { data, loading, error }] = useLazyQuery(OBTNER_USUARIOS)
+
+  useEffect(() => {
+
+    try {
+      perro()
+    } catch (error) {
+      console.log(error.message);
+    }
+
+
+  }, [])
+
+  console.log(data);
+
   return (
     <Container>
       <form>
         <div className="form-row">
           <div className="col-4">
             <div className="p-3 border bg-light">
-            <p>Nuevo Usuario</p>
+              <p>Nuevo Usuario</p>
 
               <input type="text" className="form-control" id="inputNombre" />
               <label for="inputName">Nombre</label>
 
-              <input type="text" className="form-control" id="inputApellidoP"/>
+              <input type="text" className="form-control" id="inputApellidoP" />
               <label for="inputApellidoP">Apellido Paterno</label>
 
-              <input type="text" className="form-control" id="inputApellidoM"/>
+              <input type="text" className="form-control" id="inputApellidoM" />
               <label for="inputApellidoM">Apelido Materno</label>
 
-              <input type="text" className="form-control" id="inputTelefono"/>
+              <input type="text" className="form-control" id="inputTelefono" />
               <label for="inputTelefono">Telefono</label>
 
-              <input type="email" className="form-control" id="inputEmail"/>
+              <input type="email" className="form-control" id="inputEmail" />
               <label for="inputEmail">Email</label>
 
-              <input type="password" className="form-control" id="inputPassword"/>
+              <input type="password" className="form-control" id="inputPassword" />
               <label for="inputPassword">Contrasena</label>
 
               <form>
@@ -61,31 +93,39 @@ const AgregarUsuarios = () => {
               <table class="table table-striped">
                 <thead class="thead-dark">
                   <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
+                    <th>#</th>
+                    <th>Nombre</th>
+                    <th>Apellido paterno</th>
+                    <th>Apellido materno</th>
+                    <th>Telefono</th>
+                    <th>email</th>
+
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                  </tr>
+                  {
+                    data && data.getUsuarios &&
+                    data.getUsuarios.map((usuario, index) => {
+                      return (
+                       
+                       <tr key={index}>
+                          <td>{index}</td>
+                          
+                          <td>{usuario.nombre}</td>
+                          
+                          <td>{usuario.apellidoP}</td>
+                          
+                          <td>{usuario.apellidoM}</td>
+                          
+                          <td>{usuario.telefono}</td>
+                          
+                          <td>{usuario.email}</td>
+                        </tr>
+                      )
+                    }
+                    )
+                  }
+
                 </tbody>
               </table>
             </div>
