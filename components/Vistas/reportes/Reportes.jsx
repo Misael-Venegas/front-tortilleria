2c17c74cd0250ab84d291761d26ab7e19beed6ca
evasquez <erick.marcus17@gmail.com>
2022-07-28 21:34:15 -0500

    se agrega grafica y rutas

---
+++ components/Vistas/reportes/Reportes.jsx
@@ -0,0 +1,111 @@
+import { DatePicker } from 'antd'
+import React, { useEffect, useState } from 'react'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+import {useLazyQuery, gql} from "@apollo/client";
+import moment from "moment";
+
+
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const fondos = [
+  'rgba(255, 99, 132, 0.2)',
+  'rgba(54, 162, 235, 0.2)',
+  'rgba(255, 206, 86, 0.2)',
+  'rgba(75, 192, 192, 0.2)',
+  'rgba(153, 102, 255, 0.2)',
+  'rgba(255, 159, 64, 0.2)',
+];
+
+const bordes = [
+  'rgba(255, 99, 132, 1)',
+  'rgba(54, 162, 235, 1)',
+  'rgba(255, 206, 86, 1)',
+  'rgba(75, 192, 192, 1)',
+  'rgba(153, 102, 255, 1)',
+  'rgba(255, 159, 64, 1)',
+];
+
+const OBTENER_DATOS = gql`
+  query generarReporte($desde: String, $hasta: String) {
+    generarReporte(desde: $desde, hasta: $hasta) {
+      nombres
+      ventas
+    }
+  }
+`;
+
+const Reportes = () => {
+
+  const [obtener, {loading}] = useLazyQuery(OBTENER_DATOS, {
+    onCompleted: (data) => {
+      if(data){
+        setResultados(data.generarReporte);
+      }
+    }
+  });
+
+  const [resultados, setResultados] = useState(null);
+
+  const [desde, setDesde] = useState(moment());
+  const [hasta, setHasta] = useState(moment());
+
+  useEffect(() => {
+    obtener({
+      variables: {
+        desde: desde.format('YYYY-MM-DD'),
+        hasta: hasta.format('YYYY-MM-DD')
+      }
+    })
+  }, [desde, hasta]);
+  
+
+  const datos = {
+    labels: resultados?.nombres || [],
+    datasets: [
+      {
+        label: 'Repartidores',
+        data: resultados?.ventas || [],
+        backgroundColor: fondos,
+        borderColor: bordes,
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return (
+    <>
+
+      <div className="row">
+        <div className="col-md-12">
+          <h5 className='text-muted'>Ventas por repartidor</h5>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-3">
+          <div className="form-group">
+            <label>Desde</label>
+            <DatePicker className='w-100' value={desde} onChange={(e) => setDesde(e)} />
+          </div>
+        </div>
+
+        <div className="col-md-3">
+          <div className="form-group">
+            <label>Hasta</label>
+            <DatePicker className='w-100' value={hasta} onChange={(e) => setHasta(e)}/>
+          </div>
+        </div>
+      </div>
+
+      <div className="row mt-5 text-center">
+        <div className="col-md-4">
+          <Pie data={datos} />
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default Reportes
--- components/principal/Principal.jsx
+++ components/principal/Principal.jsx
@@ -1,6 +1,6 @@
-import Layout from "../layout/Layout";
+import { useEffect, useState } from "react";
 import { Tabs } from "antd";
-import React from "react";
+import Layout from "../layout/Layout";
 import Cargo from "../Vistas/cargo/Cargo";
 import Empleados from "../Vistas/empleados/Empleados";
 import Proveedor from "../Vistas/proveedor/Proveedor";
@@ -15,77 +15,79 @@ import Sucursal from "../Vistas/sucursal/Sucursal";
 import Ventas from "../Vistas/ventas/Ventas";
 import CorteDeCaja from "../Vistas/corteDeCaja/CorteDeCaja";
 import Salidas from "../Vistas/salidas/Salidas";
+import jwtDecode from "jwt-decode";
+import Reportes from "../Vistas/reportes/Reportes";
+
+const { TabPane } = Tabs;
 
 const Principal = () => {
-  const { TabPane } = Tabs;
+  const [rol, setRol] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      const { id_cargo } = jwtDecode(token);
+      setRol(id_cargo);
+    }
+  }, []);
 
   return (
-
-    <Layout  >
-      <Tabs defaultActiveKey="1" size="small"  >
-        <TabPane tab="Ventas" key="1" >
-          {
-             <Ventas />
-          }
-        </TabPane>
-
-        <TabPane tab="Productos" key="2" >
-          <Productos />
-        </TabPane>
-        <TabPane tab="Empleados" key="3" >
-          <Empleados />
-        </TabPane>
-        <TabPane tab="Cargo" key="4" >
-          <Cargo />
-        </TabPane>
-        <TabPane tab="Corte de caja" key="5" >
-          <CorteDeCaja />
-        </TabPane>
-        <TabPane tab="Sucursal" key="6" >
-          <Sucursal />
-        </TabPane>
-        <TabPane tab="Merma" key="7" >
-          {
+    <Layout>
+      <div>
+        <Tabs defaultActiveKey="1" tabPosition="top" style={{height: 220}}>
+          <TabPane tab="Ventas" key="1">
+            <Ventas />
+          </TabPane>
+          {rol == 1 && (
+            <>
+              <TabPane tab="Productos" key="2">
+                <Productos />
+              </TabPane>
+              <TabPane tab="Empleados" key="3">
+                <Empleados />
+              </TabPane>
+              <TabPane tab="Cargo" key="4">
+                <Cargo />
+              </TabPane>
+              <TabPane tab="Corte de caja" key="5">
+                <CorteDeCaja />
+              </TabPane>
+              <TabPane tab="Sucursal" key="6">
+                <Sucursal />
+              </TabPane>
+              <TabPane tab="Salidas" key="9">
+                <Salidas />
+              </TabPane>
+              <TabPane tab="Entradas" key="10">
+                <EntradasAlmacen />
+              </TabPane>
+              <TabPane tab="Almacen" key="11">
+                <Almacen />
+              </TabPane>
+              <TabPane tab="Insumos" key="12">
+                <Insumos />
+              </TabPane>
+              <TabPane tab="Proveedores" key="13">
+                <Proveedor />
+              </TabPane>
+              <TabPane tab="Tipo almacen" key="14">
+                <TipoAlmacen />
+              </TabPane>
+              <TabPane tab="Reportes" key="15">
+                <Reportes />
+              </TabPane>
+            </>
+          )}
+          <TabPane tab="Merma" key="7">
             <Merma />
-          }
-        </TabPane>
-        <TabPane tab="Tipo merma" key="8" >
-          {
-              <TipoMerma />
-          }
-        </TabPane>
-        <TabPane tab="Salidas" key="9" >
-          <Salidas/>
-        </TabPane>
-        <TabPane tab="Entradas" key="10" >
-          {
-             <EntradasAlmacen />
-          }
-        </TabPane>
-        <TabPane tab="Almacen" key="11" >
-          {
-              <Almacen />
-          }
-        </TabPane>
-        <TabPane tab="Insumos" key="12" >
-          {
-            <Insumos />
-          }
-        </TabPane>
-        <TabPane tab="Proveedores" key="13" >
-          {
-            <Proveedor />
-          }
-        </TabPane>
-        <TabPane tab="Tipo almacen" key="14" >
-          {
-            <TipoAlmacen />
-          }
-        </TabPane>
-      </Tabs>
-
+          </TabPane>
+          <TabPane tab="Tipo merma" key="8">
+            <TipoMerma />
+          </TabPane>
+        </Tabs>
+      </div>
     </Layout>
-
   );
 };
 
