2d46550e07ce1fd7593fe73869051c6c7da6b868
Misa <misa.cc2393293@gmail.com>
2022-08-06 20:13:22 -0500

    boton actualizar

--- components/Vistas/almacen/Formulario.jsx
+++ components/Vistas/almacen/Formulario.jsx
@@ -2,14 +2,23 @@ import React, { useState, useEffect } from 'react'
 import { Button, Input } from 'antd'
 import TablaAlmacen from './TablaAlmacen'
 import ModalAgregar from './ModalAgregar'
-
+import { RetweetOutlined } from '@ant-design/icons'
 const Formulario = () => {
     const [verModal, setVerModal] = useState(false)
     const [actualizarTabla, setActualizarTabla] = useState(3.1416)
 
     return (
         <>
-            <Button className='float-right' type='primary' onClick={() => setVerModal(true)}  >Agregar producto </Button>
+            <div className='row' >
+                <div className='col-12' >
+                    <Button className='float-right' type='primary' onClick={() => setVerModal(true)}  >Agregar producto </Button>
+
+                </div>
+            </div>
+            <p style={{ fontSize: 15, color: "#40A9FF" }} className="float-right pt-2 pr-2 seleccionarComponente"
+                onClick={() => setActualizarTabla(Math.random())}>
+                Actualizar <RetweetOutlined />
+            </p>
             <div className='table-responsive'>
                 <TablaAlmacen actualizarTabla={actualizarTabla} setActualizarTabla={setActualizarTabla} />
             </div>
